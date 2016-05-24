ee4dcc9dfed950597e29328ecb44f09276cc4c98
Eduardo <eduardo.contijoch@gmail.com>
2016-05-24 11:48:14 -0400

    Slim Output Table Added

--- R/BatchFEAT.R
+++ R/BatchFEAT.R
@@ -85,7 +85,12 @@ batchFEAT <- function(biom_file, mapping_file, FMT_pairs, input_params, output_d
                        "N_P_Recipient", "R_Persist", "P_Recipient", "P_Shared", "P_Unique",
                        "D_Engraft_excl", "P_Donor_excl",
                        "R_Persist_excl", "P_Recipient_excl")
-					  
+					   
+  slim_table_out_items <- c("Comment", "TransplantID", "D_Engraft", "R_Persist", "P_Donor", "P_Recipient", "P_Shared", "P_Unique")
+  slim_table_out <- data.frame(t(slim_table_out_items))
+  colnames(slim_table_out) <- make.names(slim_table_out_items)
+  slim_table_out <- slim_table_out[-1,]
+  
   table_out <- data.frame(t(table_out_items))
   colnames(table_out) <- make.names(table_out_items)
   table_out <- table_out[-1, ]
@@ -311,11 +316,22 @@ batchFEAT <- function(biom_file, mapping_file, FMT_pairs, input_params, output_d
     pre_out <- as.data.frame(t(output_table))
     colnames(pre_out) <- make.names(Item)
     row.names(pre_out) <- NULL
+	
+	slim_items <- c("Comment", "TransplantID", "D_Engraft", "R_Persist", "P_Donor", "P_Recipient", "P_Shared", "P_Unique")
+	slim_values <- c(comment, transplant_id, D_Engraft, R_Persist, P_Donor, P_Recipient, P_Shared, P_Unique)
+	
+	slim_output <- data.frame(slim_items, slim_values)
+	slim_pre_out <- as.data.frame(t(slim_output))
+	colnames(slim_pre_out) <- make.names(slim_items)
+	row.names(slim_pre_out) <- NULL
+	
+	slim_table_out <- rbind(slim_table_out, slim_pre_out[-1,])
+	
 
     table_out <- rbind(table_out, pre_out[-1, ])
 
     cat("Writing output metric table, plot, & QC...\n")
-
+	
     write.csv(output_table, file = paste(output_name,"metric_summary_table.csv", sep = "_"), row.names = FALSE)
     metric_plot <- visualize_metrics_batch(N_otus_unique_donor, N_otus_unique_recipient, N_otus_post_fmt_full, N_P_Donor, N_P_Recipient, N_otus_unique_post_fmt, N_otus_shared_throughout, output_name, paste('Full', joining_category, output_name, sep = "_"), comment)
     metric_plot_excl <- visualize_metrics_batch(N_otus_unique_donor, N_otus_unique_recipient, N_otus_post_fmt_excl, N_P_Donor, N_P_Recipient, 0, 0, output_name, paste('Excl', joining_category, output_name, sep = "_"), comment)
@@ -371,6 +387,7 @@ batchFEAT <- function(biom_file, mapping_file, FMT_pairs, input_params, output_d
   }
 
   write.csv(table_out, file = "All_Transplant_Summary.csv", row.names = FALSE)
+  write.csv(slim_table_out, file = "Slim_All_Transplant_Summary.csv", row.names = FALSE)
   
   setwd('../')
   end_time <- proc.time()
